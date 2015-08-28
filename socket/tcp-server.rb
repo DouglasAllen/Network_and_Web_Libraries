85143f4fb8374a98790a0b836d95443229ab2d1a
Douglas G. Allen <kb9agt@gmail.com>
2015-08-28 11:44:59 -0500

    Create tcp-server.rb

---
+++ socket/tcp-server.rb
@@ -0,0 +1,15 @@
+require 'socket'
+
+server = TCPServer.new('localhost', 2345)
+
+while (session = server.accept)
+  puts "Request: #{session.gets}"
+  session.print "HTTP/1.1 200/OK\r\n" +
+                "Content-type: text/html\r\n" +
+                "\r\n"
+  session.print "<html><body>" +
+                "<h1>#{Time.now}</h1>" +
+                "</body></html>" +
+                "\r\n"
+  session.close
+end
